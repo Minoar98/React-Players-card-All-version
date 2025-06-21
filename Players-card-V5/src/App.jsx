931511ab5588b062af08028ef595617ad94a36fa
Minoar98 <minoarhasan@gmail.com>
2025-06-21 23:44:28 +0600

    Why do I write the code while updating the state which is depending on its previous state?

--- Players-card-V5/src/App.jsx
+++ Players-card-V5/src/App.jsx
@@ -43,7 +43,8 @@ function App() {
         <h1>Players Card</h1>
       </header>
       <p>count: {count}</p>
-      <button onClick={() => clickHandler(10)}>Increase</button>
+      {/* <button onClick={() => clickHandler(10)}>Increase</button> */}
+      <button onClick={clickHandler}>Increase</button>
       <section className="card-container">
         {players.map((player) => {
           // console.log(player);
@@ -55,3 +56,76 @@ function App() {
 }
 
 export default App;
+
+// ## When use `onClick={clickHandler}`?
+// Answer
+// 1. `clickHandler` is not taking any parameter
+// 2. Used function reference in `onClick`
+
+// ```js
+// const App = () => {
+//     const [count, setCount] = useState(0);
+
+//     // After clicking the `Increase` button, `clickHandler` will execute
+//     const clickHandler = () => {
+//       setCount(10); // Update the state
+//     }
+
+//     return (
+//       <button onClick={clickHandler}>Increase</button>
+//     )
+// }
+// ```
+
+// ## When use `onClick={() => clickHandler(x)}`?
+// Answer
+// 1. `clickHandler` is taking a parameter
+// 2. Used arrow function reference in `onClick`
+// 3. Inside arrow function, call the `clickHandler` function with some value
+
+// ```js
+// const App = () => {
+//     const [count, setCount] = useState(0);
+
+//     // After clicking the `Increase` button, `clickHandler` will execute
+//     const clickHandler = (cnt) => {
+//       setCount(cnt); // Update the state
+//     }
+
+//     return (
+//       <button onClick={() => clickHandler(10)}>Increase</button>
+//     )
+// }
+// ```
+
+// ## If State update depends on previous state value
+// Answer:
+// - Use `setCount((prevState) => prevState + 1)` instead of `setCount(count+1)`
+// 1. Pass a arrow function in `setCount` instead of value
+// 2. That arrow function is taking a parameter which is the previous state provided by React itself
+// 3. Now you can update the state & that arrow function must return the updated state
+
+// ## Why do I write the code while updating the state which is depending on its previous state?
+// Answer:
+// - When I use multiple state update operation, then I must ensure that for a specific state update operation which state value depends on its previous state value, I am using the right previous state value
+// - If I have the following code:
+
+// ```js
+// const clickHandler = () => {
+//   setCount(count+1);
+//   setIsLoading(false);
+//   setData(data);
+//   setValue(value);
+// }
+// ```
+
+// - Then I must write the following code only for `setCount` as `count` depends on its previous state value:
+
+// ```js
+// const clickHandler = () => {
+//   setCount((prevState) => prevState + 1); // Change here
+//   setIsLoading(false);
+//   setData(data);
+//   setValue(value);
+// }
+// ```
